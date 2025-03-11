0f20a318242b5bc69108c79e998fbc43c984250b
Souvik <souvik13109@gmail.com>
2025-03-12 01:23:07 +0530

    Update App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-3xl text-blue-500'>App</div>
+    <div className='text-3xl flex mt-52 font-bold items-center justify-center text-blue-500'>AuthBase</div>
   )
 }
 
