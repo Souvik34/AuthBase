e28014565f9e068e03722511fc917028b73f2f4b
Souvik <souvik13109@gmail.com>
2025-03-12 01:37:07 +0530

    Add MongoDB connection and user model

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app= express();
 require('dotenv').config()
 
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB...');
+}).catch((err) => {
+    console.log(err);
+});
+
 const PORT= process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
