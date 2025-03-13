a8ddc6ca9709e9f8c55b598600e3de9b47f81dc2
Souvik <souvik13109@gmail.com>
2025-03-13 20:55:56 +0530

    Add routing and page components for login, signup, and home; refactor component names to PascalCase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/login'
+import Signup from './pages/signup'
+import Home from './pages/home'
 
 const App = () => {
   return (
+    <>
     <div className='text-3xl flex mt-52 font-bold items-center justify-center text-blue-500'>AuthBase</div>
+    <Routes>
+
+      <Route path='/' element={<Navigate to= '/login'/>} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/home' element={<Home />} />
+
+    </Routes>
+    </>
   )
 }
 
--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const signup = () => {
+const Signup = () => {
   return (
-    <div>signup</div>
+   <div className='container'>
+    <h1>Signup</h1>
+    <form>
+        <div>
+            <label htmlFor="name">Name</label>
+            <input
+             type="text"
+             name='name'
+             autoFocus
+             placeholder='Enter your Name'
+              />
+        </div>
+    </form>
+   </div>
   )
 }
 
-export default signup
+export default Signup
